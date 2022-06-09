9b34df799f32cfe308552a7c721a53c06eb98be1
Felipe1501 <felipebsantos1501@gmail.com>
2022-06-09 11:20:29 -0300

    adcionando novo site do meu TCC ANIMAL POINT

---
+++ animal Point/assets/script/js/index.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body"),
+searchToggle = document.querySelector(".searchToggle"),
+pesquisar = document.querySelector(".pesquisar-botao");
+
+searchToggle.addEventListener("click", () => {
+searchToggle.classList.toggle("ativar");
+});

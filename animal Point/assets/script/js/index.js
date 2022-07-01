72a2c623f527f726280dd206bd8bdf8fb0261dde
Felipe1501 <felipebsantos1501@gmail.com>
2022-07-01 12:31:31 -0300

    adicionando projeto novo de forms

--- animal Point/assets/script/js/index.js
+++ animal Point/assets/script/js/index.js
@@ -5,3 +5,16 @@ pesquisar = document.querySelector(".pesquisar-botao");
 searchToggle.addEventListener("click", () => {
 searchToggle.classList.toggle("ativar");
 });
+
+var $simpleCarousel = document.querySelector(".js-carousel--simple");
+ 
+new Glider($simpleCarousel, {
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  draggable: true,
+  dots: ".js-carousel--simple-dots",
+  arrows: {
+    prev: ".js-carousel--simple-prev",
+    next: ".js-carousel--simple-next",
+  },
+});
